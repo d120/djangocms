8611bf1c6933e63df16e6c9de19ec2e8372c4403
Jannik Vieten <jannik.vieten@fachschaft.informatik.tu-darmstadt.de>
2016-04-17 13:24:39 +0200

    added production settings
    - set allowed hosts (does not harm when debug=True)
    - added settings_production module for production settings
    - added mysqlclient to requirements.txt

---
+++ d120/settings_production.py
@@ -0,0 +1,22 @@
+"""
+This is the settings file used in production.
+First, it imports all default settings, then overrides respective ones.
+Secrets are stored in and imported from an additional file, not set under version control.
+"""
+
+from d120.settings import *
+import d120.settings_secrets as secrets
+
+SECRET_KEY = secrets.SECRET_KEY
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': 'djangocms',
+        'USER': 'djangocms',
+        'PASSWORD': secrets.DB_PASSWORD
+    }
+}
