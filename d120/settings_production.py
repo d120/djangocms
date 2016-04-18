b739051260452baa6c7c44f3fc59a84419608c7e
Jannik Vieten <jannik.vieten@fachschaft.informatik.tu-darmstadt.de>
2016-04-18 14:37:07 +0200

    added LDAP configuration to production settings
    - can authenticate users
    - mirrors groups
    - adopts user attributes
    - sets flags appropriately

--- d120/settings_production.py
+++ d120/settings_production.py
@@ -4,6 +4,9 @@ First, it imports all default settings, then overrides respective ones.
 Secrets are stored in and imported from an additional file, not set under version control.
 """
 
+import ldap
+from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
+
 from d120.settings import *
 import d120.settings_secrets as secrets
 
@@ -24,3 +27,25 @@ DATABASES = {
 SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
+
+AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+AUTH_LDAP_SERVER_URI = "ldap://10.162.32.201"
+AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s,ou=People,dc=fachschaft,dc=informatik,dc=tu-darmstadt,dc=de"
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=Group,dc=fachschaft,dc=informatik,dc=tu-darmstadt,dc=de", ldap.SCOPE_SUBTREE, "(objectClass=groupOfNames)")
+AUTH_LDAP_GROUP_TYPE = GroupOfNamesType()
+AUTH_LDAP_MIRROR_GROUPS = True
+AUTH_LDAP_REQUIRE_GROUP = "cn=fachschaft,ou=Group,dc=fachschaft,dc=informatik,dc=tu-darmstadt,dc=de"
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail",
+}
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+    "is_active": "cn=fachschaft,ou=Group,dc=fachschaft,dc=informatik,dc=tu-darmstadt,dc=de",
+    "is_staff": "cn=fachschaft,ou=Group,dc=fachschaft,dc=informatik,dc=tu-darmstadt,dc=de",
+    "is_superuser": "cn=fss,ou=Group,dc=fachschaft,dc=informatik,dc=tu-darmstadt,dc=de",
+}
