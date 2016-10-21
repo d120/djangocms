5713bfc543c4806fee74770a131ca2a0d6473b7b
Jannik Vieten <jannik.vieten@fachschaft.informatik.tu-darmstadt.de>
2016-10-21 13:38:08 +0200

    added a menu modifier which makes the page_title accessible inside menu templates

--- d120/cms_menus.py
+++ d120/cms_menus.py
@@ -4,6 +4,21 @@ from menus.menu_pool import menu_pool
 from cms.models import Page
 
 
+class PageTitleModifier(Modifier):
+    """Make the page_title accessible for the menu.
+    """
+    def modify(self, request, nodes, namespace, root_id, post_cut, breadcrumb):
+        if post_cut:
+            return nodes
+        for node in nodes:
+            if node.attr["is_page"]:
+                page = Page.objects.get(id=node.id)
+                node.attr["page_title"] = page.get_page_title()
+        return nodes
+
+menu_pool.register_modifier(PageTitleModifier)
+
+
 class MenuEntryMarginModifier(Modifier):
     """Make the additional menu entry margin accessible for the menu.
     """
