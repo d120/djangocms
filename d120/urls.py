4c830f59e9e63faaf23fa06e5f39ff026d0d7ee6
Jannik Vieten <jannik.vieten@fachschaft.informatik.tu-darmstadt.de>
2016-12-11 16:07:18 +0100

    fixed deprecated url config

--- d120/urls.py
+++ d120/urls.py
@@ -7,13 +7,13 @@ from django.conf.urls import *  # NOQA
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import django.contrib.sitemaps.views
 
 admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
+urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),  # NOQA
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
-        {'sitemaps': {'cmspages': CMSSitemap}}),
+    url(r'^sitemap\.xml$', django.contrib.sitemaps.views.sitemap, {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^select2/', include('django_select2.urls')),
     url('^markdown/', include('django_markdown.urls')),
     url(r'^', include('cms.urls')),
@@ -21,7 +21,7 @@ urlpatterns = i18n_patterns('',
 
 # This is only needed when using runserver.
 if settings.DEBUG:
-    urlpatterns = patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',  # NOQA
-            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        ) + staticfiles_urlpatterns() + urlpatterns  # NOQA
+    import django.views.static
+    urlpatterns = [
+        url(r'^media/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        ] + staticfiles_urlpatterns() + urlpatterns
