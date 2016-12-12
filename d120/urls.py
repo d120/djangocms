8a7fdfce9df97519f95d3c25311ccc347eca4c36
Jannik Vieten <jannik.vieten@fachschaft.informatik.tu-darmstadt.de>
2016-12-12 11:10:58 +0100

    fixed more url warnings

--- d120/urls.py
+++ d120/urls.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
-from django.conf.urls import *  # NOQA
+from django.conf.urls import url, include
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -12,7 +12,7 @@ import django.contrib.sitemaps.views
 admin.autodiscover()
 
 urlpatterns = i18n_patterns(
-    url(r'^admin/', include(admin.site.urls)),  # NOQA
+    url(r'^admin/', admin.site.urls),
     url(r'^sitemap\.xml$', django.contrib.sitemaps.views.sitemap, {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^select2/', include('django_select2.urls')),
     url('^markdown/', include('django_markdown.urls')),
