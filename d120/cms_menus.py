67a73a8a70238b0378e7e9c9bec013706e6de984
Jannik Vieten <jannik.vieten@fachschaft.informatik.tu-darmstadt.de>
2016-12-24 12:46:17 +0100

    since a9256d3e accelerated the menu modifier, it no longer took the language into account, this patch fixes this behavior

--- d120/cms_menus.py
+++ d120/cms_menus.py
@@ -1,36 +1,48 @@
+from django.utils.translation import get_language
+from cms.models import Title
 from menus.base import Modifier
 from menus.menu_pool import menu_pool
-
-from cms.models import Title
 
 
 class D120NavigationModifier(Modifier):
     """Make several useful attributes accessible for the menu system.
+    - the page_title attribute
+    - the custom MenuEntryMarginExtension
+    - the custom MenuEntryHeadlineExtension
     """
     def modify(self, request, nodes, namespace, root_id, post_cut, breadcrumb):
 
         if breadcrumb:
-            selected = next(n for n in nodes if n.selected and n.attr["is_page"])
-            page_nodes = [selected] + selected.get_ancestors()
-            titles = {t.page_id: t for t in Title.objects.filter(page_id__in=[n.id for n in page_nodes])}
-            for node in page_nodes:
+            language = get_language()
+            # take the nodes relevant for the breadcrumb
+            selected_node = next(n for n in nodes if n.selected and n.attr["is_page"])
+            page_nodes = [selected_node] + selected_node.get_ancestors()
+            nodes_by_id = {n.id: n for n in page_nodes}
+            # fetch relevant fields from the database
+            titles = Title.objects.filter(page_id__in=[n.id for n in page_nodes]).values('page_id', 'language', 'page_title', 'title')
+
+            for t in titles:
+                node = nodes_by_id[t['page_id']]
                 # make the page_title attribute accessible for the breadcrumb
-                node.attr["page_title"] = titles[node.id].page_title if titles[node.id].page_title else titles[node.id].title
+                if language == t['language'] or 'page_title' not in node.attr:
+                    node.attr['page_title'] = t['page_title'] if t['page_title'] else t['title']
 
         elif post_cut:
+            language = get_language()
+            # take the nodes relevant for the menu
             page_nodes = [n for n in nodes if n.attr["is_page"]]
-            titles = {t.page_id: t for t in Title.objects.filter(page_id__in=[n.id for n in page_nodes]).select_related('page__menuentrymarginextension', 'menuentryheadlineextension')}
-            for node in page_nodes:
-                title = titles[node.id]
-                page = title.page
+            nodes_by_id = {n.id: n for n in page_nodes}
+            # fetch relevant fields from the database
+            titles = Title.objects.filter(page_id__in=[n.id for n in page_nodes]).values('page_id', 'language', 'page__menuentrymarginextension__additional_margin', 'menuentryheadlineextension__headline')
 
+            for t in titles:
+                node = nodes_by_id[t['page_id']]
                 # make the custom MenuEntryMarginExtension accessible for the menu
-                if hasattr(page, "menuentrymarginextension"):
-                    node.attr["additional_margin"] = page.menuentrymarginextension.additional_margin
+                node.attr['additional_margin'] = t['page__menuentrymarginextension__additional_margin']
 
                 # make the custom MenuEntryHeadlineExtension accessible for the menu
-                if hasattr(title, "menuentryheadlineextension"):
-                    node.attr["headline"] = title.menuentryheadlineextension.headline
+                if t['menuentryheadlineextension__headline'] and (language == t['language'] or 'headline' not in node.attr):
+                    node.attr['headline'] = t['menuentryheadlineextension__headline']
 
         return nodes
 
