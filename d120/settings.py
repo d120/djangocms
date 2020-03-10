d764467fae89fe94983925127c1aa8c4de527137
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-03-10 17:48:27 +0100

    reactivate djangocms-inherit

--- d120/settings.py
+++ d120/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = (
 #    'cmsplugin_filer_teaser',
 #    'cmsplugin_filer_utils',
 #    'cmsplugin_filer_video',
-#    'djangocms_inherit',
+    'djangocms_inherit',
     'djangocms_redirect',
     'djangocms_history',
 #    'aldryn_bootstrap3',
