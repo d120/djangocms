b24f2d3ddefb2baa0cd0ce4c864bfe3bdf2bbfa3
Xiphoseer <Xiphoseer@users.noreply.github.com>
2020-10-24 17:53:37 +0200

    Add website CSS to ckeditor

--- d120/settings.py
+++ d120/settings.py
@@ -275,6 +275,12 @@ CKEDITOR_SETTINGS = {
     'skin': 'moono-lisa',
     'toolbar': 'CMS',
     'stylesSet': format_lazy('default:{}', reverse_lazy('admin:cascade_texteditor_config')),
+    'contentsCss': [
+        '/static/vendor/bootstrap/dist/css/bootstrap.min.css',
+        '/static/vendor/font-awesome/css/font-awesome.min.css',
+        '/static/vendor/typeface-libre-franklin/index.css',
+        '/static/d120/css/custom.css',
+    ],
 }
 
 CMSPLUGIN_CASCADE = {
