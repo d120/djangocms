0a4ee54bfc076173b4cea0212cbdf9b65e94c708
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2020-10-25 16:56:38 +0100

    Allow to add links and styling to ToDo entries
    Introduce bleach to requirements to filter html tags in ToDo entries
    Add new template tag to clean and mark that cleaned html string as safe
    Fix linebreaks for very long to do items

--- d120/templatetags/d120_cascade.py
+++ d120/templatetags/d120_cascade.py
@@ -1,4 +1,6 @@
+import bleach as bleach
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -15,3 +17,12 @@ def to_todo_item_list(items_str, prefix):
 @register.filter
 def multiline_to_items(items_str):
     return items_str.splitlines()
+
+
+@register.filter
+def partially_safe(midsafe_string):
+    """
+    Clean string from everything not allowed by default by bleach lib
+    and mark the resulting string as safe
+    """
+    return mark_safe(bleach.clean(midsafe_string))
