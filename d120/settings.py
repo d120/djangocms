e3821692a7eb6e336351f2c0845fb2d84f814254
Xiphoseer <Xiphoseer@users.noreply.github.com>
2020-11-04 00:15:04 +0100

    Remove djangocms-inherit

--- d120/settings.py
+++ d120/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = (
     'cmsplugin_filer_teaser',
     'cmsplugin_filer_utils',
     'cmsplugin_filer_video',
-    'djangocms_inherit',
     'djangocms_redirect',
     'djangocms_history',
     'djangocms_icon',
